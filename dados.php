dcbae2b2366f8df0290e7eb26e4b8f0e535ced64
Daniels Hogans <daniel.santos.ap@gmail.com>
2023-09-14 22:31:22 -0300

    start

---
+++ dados.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>cade os dados?<p>
+
+    <p>ola. seja bem vindo, <?php echo $_GET["p_nome"]?></p>
+    <p>seua idade é de : <?php echo $_GET["p_idade"]?></p>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>001 - pagina</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1 class="">estude!</h1>
+        <p>eu estudo PHP a mais de 20 anos</p>
+        <h2>PHP</h2>
+        <h3>em casa </h3>
+        <h4 class="h6 text-warning">josé</h4>
+        <form action="dados.php" method="get">
+            <label>digite nome:</label><input type="text" name="p_nome"><br/>
+            <label>digite idade:</label><input type="text" name="p_idade"><br/>
+            <input type="submit" value="mandar">
+        </form>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
+
